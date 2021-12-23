78862e1ffa5f411f10f0a38aa1e2288cd99a5947
rotariucatalin <rotariucatalin90@gmail.com>
2021-12-23 11:33:31 +0200

    Initial commit. Adding MySQL connection properties

---
+++ src/main/java/com/example/plannerrestapi/PlannerRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.plannerrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerRestApiApplication.class, args);
+	}
+
+}
